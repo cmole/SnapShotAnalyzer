b073c9c5dc98faedf5f6e3cb0312377fdd0a3bdf
Christian Mueller <cmole9@gmail.com>
2020-04-26 12:30:06 -0400

    Added start, stop commands

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# SnapShotAnalyzer
+## SnapShotAnalyzer
 Tool to manage EC2 snapshots and instances
 
-# Running
+## About
+
+This is a demo project to manage EC2 instances & snapshots.  It uses the boto3 tool to connect to AWS
+
+## Configuring
+shotty uses the configuration file created by the AWS cli e.g.
+
+`aws configure -- profile shotty`
+
+## Running
 This requires PipEnv to execute
 
-```
-pinenv install
-pipenv run python "shotty/shotty.py command"
-````
+`pinenv install`
+`pipenv run python shotty/shotty.py <command> <--project=PROJECT>`
+
+*command* is list, start or stop
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,21 +1,64 @@
 import boto3
 import click
 
+
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_sessions():
+def getInstances(project):
+    instances = []
+    
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help='Only instances for project (tag Project:<name>)')
+def list_sessions(project):
     "List EC2 Sessions"
-    for i in ec2.instances.all():
+    
+    instances = getInstances(project)
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
         print(', '.join((
             i.id,
             i.instance_type,
             i.placement['AvailabilityZone'],
             i.state['Name'],
+            tags.get('Project', '<no project>'),
             i.public_dns_name))
         )
     
+@instances.command('stop')
+@click.option('--project', default=None,
+    help='Only instances for project (tag Project:<name>)')
+def stop_sessions(project):
+    "Stop EC2 Sessions"
+
+    instances = getInstances(project)
+    for i in instances:
+        i.stop()
+
+@instances.command('start')
+@click.option('--project', default=None,
+    help='Only instances for project (tag Project:<name>)')
+def start_sessions(project):
+    "Start EC2 Sessions"
+
+    instances = getInstances(project)
+    for i in instances:
+        i.start()
 
 if __name__ == '__main__':
-    list_sessions()
+    instances()
+    instances()
+instances
