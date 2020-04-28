5efe370d973e3e0b179bc4373b3f0081c995c3da
Christian Mueller <cmole9@gmail.com>
2020-04-28 09:55:46 -0400

    Added messaging & waiting for stop/start of servers

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -68,6 +68,25 @@ def start_sessions(project):
         i.start()
     return
 
+@instances.command('snapshot')
+@click.option('--project', default=None,
+    help='Only instances for project (tag Project:<name>)')
+def create_snapshot(project):
+    "Create snapshots for EC2 instances"
+
+    instances = getInstances(project)
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("  Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by snappy")
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
+    return
+
 
 @cli.group('volumes')
 def volumes():
